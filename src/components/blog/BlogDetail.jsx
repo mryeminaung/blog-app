8d26dee228353af4e401fc66be727ab7e13ef3a7
mryeminaung <yeminaung013@gmail.com>
2024-02-12 20:42:26 +0630

    create blog detail ui for each blog

--- src/components/blog/BlogDetail.jsx
+++ src/components/blog/BlogDetail.jsx
@@ -1,16 +1,50 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const BlogDetail = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const blog = location.state;
 
   return (
-    <div>
-      <h2 className="font-bold text-4xl mb-3">
-        Blog Detail for blogId : {blog.id}
-      </h2>
-      <p className="text-2xl">{JSON.stringify(blog)}</p>
-    </div>
+    blog && (
+      <div className="flex items-start py-10 justify-center">
+        <div className="w-[600px] bg-slate-200 border border-slate-300 p-3 rounded-md space-y-3 relative">
+          <button
+            className="hover:bg-slate-600 bg-slate-500 text-white px-4 py-1 rounded-md"
+            onClick={() => navigate("/blogs")}
+          >
+            Go Back
+          </button>
+          <div className="absolute bg-slate-300 rounded-md px-3 py-1 right-5 top-[60px]">
+            {blog.category}
+          </div>
+          <img src={blog.blog_img} alt="" className="rounded-lg w-full" />
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-x-2">
+              <img
+                src={blog.blog_img}
+                alt=""
+                className="object-fit w-12 h-12 rounded-full"
+              />
+              <div className="flex flex-col w-20 -space-y-1 text-[14px]">
+                <span className="font-semibold">{blog.author}</span>
+                <span className="">{blog.created_at}</span>
+              </div>
+            </div>
+            <div className="space-x-3">
+              <button className="px-3 py-1 bg-slate-400 rounded-md">
+                Edit
+              </button>
+              <button className="px-3 py-1 bg-slate-400 rounded-md">
+                Delete
+              </button>
+            </div>
+          </div>
+          <h2 className="text-3xl font-bold">{blog.title}</h2>
+          <p className="text-xl">{blog.body}</p>
+        </div>
+      </div>
+    )
   );
 };
 
